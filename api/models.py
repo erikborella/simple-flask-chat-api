f524cbdd50618c078c4adb53e352096f5cccf247
Erik Borella <erikborella@gmail.com>
2020-01-09 12:37:09 -0300

    User model create and db configured

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+
+    image_path = db.Column(db.String(500), nullable=True)
+
+    def __init__(self, name: str, email: str, password: str):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "<User: %r:%r>" % (self.name, self.email)
