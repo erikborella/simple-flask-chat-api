c751f6d4acc765716d6c87ca154fb45b1724d6ed
Erik Borella <erikborella@gmail.com>
2020-01-09 21:47:50 -0300

    email is now unique

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.String(500), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
 
     image_path = db.Column(db.String(500), nullable=True)
