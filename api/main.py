8e0c705caa33a7836b08ad95f3d3b11900a4f19b
Erik Borella <erikborella@gmail.com>
2020-01-23 21:30:22 -0300

    removed HelloWord resource

--- api/main.py
+++ api/main.py
@@ -1,14 +1,12 @@
 from app_creator import create_app
 from flask_restful import Api
 
-from resources.HelloWorld import HelloWorld
 from resources.user_resource import Users, GetOneUser
 
 
 app = create_app()
 api = Api(app)
 
-api.add_resource(HelloWorld, '/')
 api.add_resource(Users, '/api/user')
 api.add_resource(GetOneUser, '/api/user/<user_id>')
 
