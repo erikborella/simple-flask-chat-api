7606fc758fad4e187bc78623204e6ff24f978e74
Erik Borella <erikborella@gmail.com>
2020-01-23 21:14:29 -0300

    auth renamed to user_resource

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from app_creator import create_app
 from flask_restful import Api
 
 from resources.HelloWorld import HelloWorld
-from resources.auth import Users, GetOneUser
+from resources.user_resource import Users, GetOneUser
 
 
 app = create_app()
