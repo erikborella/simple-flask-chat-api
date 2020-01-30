e91f83612c330021993eaba4e13029cae78af791
Erik Borella <erikborella@gmail.com>
2020-01-30 15:47:15 -0300

    removed all __pychache__ files

--- api/config.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'b60bb34c42b307171bdb88b10191d62739dd0da7e1e0c892'
-
-# Set sqlite database file
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database/db.db'
-
-ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg'])
-UPLOAD_FOLDER = 'public/'
