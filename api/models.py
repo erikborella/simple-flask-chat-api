3df3d807f1f03d05568ef36184858ec92f29af18
Erik Borella <erikborella@gmail.com>
2020-01-30 18:10:34 -0300

    add password and unique name in room model

--- api/models.py
+++ api/models.py
@@ -20,10 +20,12 @@ class User(db.Model):
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, password: str):
         self.name = name
+        self.password = password
 
     def __repr__(self):
         return "<Room: %r>" % self.name
--- api/resources/room_resource.py
+++ api/resources/room_resource.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource
 
+from werkzeug.security import generate_password_hash
+
 from models import Room, Participant
 from models_schemas import room_schema, participant_schema
 
@@ -10,18 +12,24 @@ from utils.auth import token_required
 
 class Rooms(Resource):
 
+    def is_room_name_already_in_use(self, name: str) -> bool:
+        return Room.query.filter(Room.name == name).first() is not None
+
     @token_required
-    @check_fields(fields=("name",))
+    @check_fields(fields=("name", "password"))
     def post(self, **kwargs):
 
         user = kwargs.get('user')
         fields = kwargs.get('fields')
 
         name = fields.get('name')
+        password = generate_password_hash(fields.get('password'))
 
-        room = Room(name)
+        if self.is_room_name_already_in_use(name):
+            return {'error': 'Already exist a room with this name'}, 400
+
+        room = Room(name, password)
         participant = Participant(user, room)
-        print(participant)
 
         try:
             db.session.add(room)
