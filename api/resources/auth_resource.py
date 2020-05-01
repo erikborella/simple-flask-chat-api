698070c92a3db47d517cded87b1a61c9f448b4ee
Erik Borella <erikborella@gmail.com>
2020-05-01 11:34:53 -0300

    access token is not set in cookies

--- api/resources/auth_resource.py
+++ api/resources/auth_resource.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 
-from flask import request
+from flask import request, make_response
 
 from utils.auth import auth, token_required
 
@@ -10,7 +10,11 @@ post: authenticates the user and return a authenciation token
 """
 class Auth(Resource):
 
-    def post(self):
-        return {
-            'token': auth()
-        }
+    def post(self):        
+        token = auth()
+
+        response = make_response({'message': 'successfuly loged'})
+
+        response.set_cookie('access_token', token, httponly=True)
+
+        return response
